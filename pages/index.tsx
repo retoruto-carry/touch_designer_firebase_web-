033485787cdbd14738f9e01757dc1a5942c4173c
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2022-12-04 14:18:54 +0900

    feat: ハートボタンとスタンプ

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,37 @@ import {push, ref} from "firebase/database";
 import {database} from "../firebase/index"
 
 export default function HomePage() {
-  const dbRef = ref(database, `messages`);
-  const newMessageRef = push(dbRef)
-  const mutation = useDatabaseSetMutation(newMessageRef);
+  const dbHeartRef = ref(database, `hearts`);
+  const newHeartRef = push(dbHeartRef)
+  const heartMutation = useDatabaseSetMutation(newHeartRef);
+
+  const dbStampRef = ref(database, `stamps`);
+  const newStampRef = push(dbStampRef)
+  const stampMutation = useDatabaseSetMutation(newStampRef);
 
   return (
     <Container>
       <Center h={'100vh'}>
         <Box
           onClick={() => {
-            mutation.mutate({name: "retca"})
+            heartMutation.mutate({value: 1})
+          }}
+          _focus={{boxShadow: 'none'}}
+          as={motion.button}
+          whileHover={{scale: 1.1}}
+          whileTap={{scale: 0.9}}
+          cursor={'pointer'}
+        >
+          <Image
+            boxSize="150px"
+            objectFit="cover"
+            src="/images/heart.png"
+            alt={'send heat'}
+          />
+        </Box>
+        <Box
+          onClick={() => {
+            stampMutation.mutate({value: 1})
           }}
           _focus={{boxShadow: 'none'}}
           as={motion.button}
@@ -27,7 +48,7 @@ export default function HomePage() {
             boxSize="150px"
             objectFit="cover"
             src="https://pbs.twimg.com/profile_images/1362720299674861570/fDwmsAK0_400x400.png"
-            alt={'push me'}
+            alt={'send stamp 1'}
           />
         </Box>
       </Center>
